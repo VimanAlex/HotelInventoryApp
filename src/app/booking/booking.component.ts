5f375860ee30ee8b867b3739b482ac0c11c22238
Viman Alex <alexandruviman@programmingpool.com>
2023-11-09 12:08:55 +0200

    test

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'hinv-booking',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent {
+
+  bookingForm!:FormGroup;
 
 }
